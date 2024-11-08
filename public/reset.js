1fed15581a27957cba7b42f7f9a507734bf1f658
Josh Hipkin <jhipkin@my.bcit.ca>
2024-11-07 23:34:51 -0800

    logs

--- public/reset.js
+++ public/reset.js
@@ -6,7 +6,9 @@ document
     event.preventDefault();
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get("token"); // Get the token from URL
+    console.log(token);
     const newPassword = document.getElementById("newPassword").value;
+    console.log(newPassword);
 
     try {
       const response = await fetch(`${endpointUrl}/api/v1/update-password`, {
