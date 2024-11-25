d36d2c556b16cc98f2c25d74475de8338cf38a67
EunjeongHur <ungdunge2@gmail.com>
2024-11-25 12:18:33 -0800

    change

--- public/index.js
+++ public/index.js
@@ -1,125 +1,131 @@
-// const endpointUrl = "https://isa-c4p-4vqm.onrender.com";
-const endpointUrl = "https://localhost:8080";
+const endpointUrl = "https://isa-c4p-4vqm.onrender.com";
+// const endpointUrl = "https://localhost:8080";
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("welcome-text").textContent = messages.welcome;
-  document.getElementById("signup-text").textContent = messages.signup;
-  document.getElementById("signup-text2").textContent = messages.signup;
-  document.getElementById("email-text").textContent = messages.email;
-  document.getElementById("password-text").textContent = messages.password;
-  document.getElementById("login-text").textContent = messages.login;
-  document.getElementById("email-text2").textContent = messages.email;
-  document.getElementById("password-text2").textContent = messages.password;
-  document.getElementById("login-text2").textContent = messages.login;
-  document.getElementById("forgot-password-link").textContent = messages.forgotPassword;
-  document.getElementById("resetPassword-text").textContent = messages.resetPassword;
-  document.getElementById("email-text3").textContent = messages.email;
-  document.getElementById("resetPassword-text2").textContent = messages.resetPassword;
-  document.getElementById("back-to-login").textContent = messages.backToLogin;
+	document.getElementById("welcome-text").textContent = messages.welcome;
+	document.getElementById("signup-text").textContent = messages.signup;
+	document.getElementById("signup-text2").textContent = messages.signup;
+	document.getElementById("email-text").textContent = messages.email;
+	document.getElementById("password-text").textContent = messages.password;
+	document.getElementById("login-text").textContent = messages.login;
+	document.getElementById("email-text2").textContent = messages.email;
+	document.getElementById("password-text2").textContent = messages.password;
+	document.getElementById("login-text2").textContent = messages.login;
+	document.getElementById("forgot-password-link").textContent =
+		messages.forgotPassword;
+	document.getElementById("resetPassword-text").textContent =
+		messages.resetPassword;
+	document.getElementById("email-text3").textContent = messages.email;
+	document.getElementById("resetPassword-text2").textContent =
+		messages.resetPassword;
+	document.getElementById("back-to-login").textContent = messages.backToLogin;
 });
 
 // Toggle the password reset form visibility
 document
-  .getElementById("forgot-password-link")
-  .addEventListener("click", (event) => {
-    event.preventDefault();
-    document.getElementById("login-section").classList.add("d-none");
-    document
-      .getElementById("reset-password-section")
-      .classList.remove("d-none");
-  });
+	.getElementById("forgot-password-link")
+	.addEventListener("click", (event) => {
+		event.preventDefault();
+		document.getElementById("login-section").classList.add("d-none");
+		document
+			.getElementById("reset-password-section")
+			.classList.remove("d-none");
+	});
 
 // Back to login button in the reset password section
 document.getElementById("back-to-login").addEventListener("click", (event) => {
-  event.preventDefault();
-  document.getElementById("reset-password-section").classList.add("d-none");
-  document.getElementById("login-section").classList.remove("d-none");
+	event.preventDefault();
+	document.getElementById("reset-password-section").classList.add("d-none");
+	document.getElementById("login-section").classList.remove("d-none");
 });
 
 // Handle Password Reset Form submission
 document
-  .getElementById("reset-password-form")
-  .addEventListener("submit", async (event) => {
-    event.preventDefault();
-    const email = document.getElementById("reset-email").value;
+	.getElementById("reset-password-form")
+	.addEventListener("submit", async (event) => {
+		event.preventDefault();
+		const email = document.getElementById("reset-email").value;
 
-    try {
-      const response = await fetch(`${endpointUrl}/api/v1/password-reset`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email }),
-      });
+		try {
+			const response = await fetch(
+				`${endpointUrl}/api/v1/password-reset`,
+				{
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({ email }),
+				}
+			);
 
-      const data = await response.json();
-      document.getElementById("resetMessage").textContent = response.ok
-        // ? "If this email is registered, you will receive password reset instructions."
-        ? messages.resetPasswordInstructions
-        // : data.message || "Unable to process password reset.";
-        : data.message || messages.unableToProcessPasswordReset;
-    } catch (error) {
-      console.error("Error:", error);
-      document.getElementById("resetMessage").textContent =
-        "An error occurred. Please try again.";
-    }
-  });
+			const data = await response.json();
+			document.getElementById("resetMessage").textContent = response.ok
+				? // ? "If this email is registered, you will receive password reset instructions."
+				  messages.resetPasswordInstructions
+				: // : data.message || "Unable to process password reset.";
+				  data.message || messages.unableToProcessPasswordReset;
+		} catch (error) {
+			console.error("Error:", error);
+			document.getElementById("resetMessage").textContent =
+				"An error occurred. Please try again.";
+		}
+	});
 
 // Handle login form submission
 document
-  .getElementById("login-form")
-  .addEventListener("submit", async (event) => {
-    event.preventDefault();
+	.getElementById("login-form")
+	.addEventListener("submit", async (event) => {
+		event.preventDefault();
 
-    const email = document.getElementById("login-email").value;
-    const password = document.getElementById("login-password").value;
+		const email = document.getElementById("login-email").value;
+		const password = document.getElementById("login-password").value;
 
-    try {
-      const response = await fetch(`${endpointUrl}/api/v1/login`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-        credentials: "include", // Include credentials for cookies
-      });
+		try {
+			const response = await fetch(`${endpointUrl}/api/v1/login`, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ email, password }),
+				credentials: "include", // Include credentials for cookies
+			});
 
-      const data = await response.json();
-      document.getElementById("responseMessage").textContent = response.ok
-        ? (window.location.href = "/main")
-        // : data.message || "Login Failed";
-        : data.message || messages.loginFailed;
-    } catch (error) {
-      console.error("Error:", error);
-      document.getElementById("responseMessage").textContent =
-        // "An error occurred. Please try again or reset your password.";
-        messages.errorOccurred;
-    }
-  });
+			const data = await response.json();
+			document.getElementById("responseMessage").textContent = response.ok
+				? (window.location.href = "/main")
+				: // : data.message || "Login Failed";
+				  data.message || messages.loginFailed;
+		} catch (error) {
+			console.error("Error:", error);
+			document.getElementById("responseMessage").textContent =
+				// "An error occurred. Please try again or reset your password.";
+				messages.errorOccurred;
+		}
+	});
 
 // Handle signup form submission
 document
-  .getElementById("signup-form")
-  .addEventListener("submit", async (event) => {
-    event.preventDefault();
+	.getElementById("signup-form")
+	.addEventListener("submit", async (event) => {
+		event.preventDefault();
 
-    const email = document.getElementById("signup-email").value;
-    const password = document.getElementById("signup-password").value;
+		const email = document.getElementById("signup-email").value;
+		const password = document.getElementById("signup-password").value;
 
-    try {
-      const response = await fetch(`${endpointUrl}/api/v1/signup`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
+		try {
+			const response = await fetch(`${endpointUrl}/api/v1/signup`, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ email, password }),
+			});
 
-      const data = await response.json();
-      document.getElementById("responseMessage").textContent = response.ok
-        // ? "Signup successful! You can now log in."
-        ? messages.signupSuccessful
-        // : data.message || "Signup failed. Please try again.";
-        : data.message || messages.signupFailed;
-      if (response.ok) document.getElementById("signup-form").reset();
-    } catch (error) {
-      console.error("Error:", error);
-      document.getElementById("responseMessage").textContent =
-        // "An error occurred. Please try again.";
-        messages.errorOccurred2;
-    }
-  });
+			const data = await response.json();
+			document.getElementById("responseMessage").textContent = response.ok
+				? // ? "Signup successful! You can now log in."
+				  messages.signupSuccessful
+				: // : data.message || "Signup failed. Please try again.";
+				  data.message || messages.signupFailed;
+			if (response.ok) document.getElementById("signup-form").reset();
+		} catch (error) {
+			console.error("Error:", error);
+			document.getElementById("responseMessage").textContent =
+				// "An error occurred. Please try again.";
+				messages.errorOccurred2;
+		}
+	});
--- server.js
+++ server.js
@@ -1,91 +1,62 @@
 const http = require("http");
-const https = require("https");
 const fs = require("fs");
 const path = require("path");
 
 const PORT = process.env.PORT || 3030;
 const publicDirectory = path.join(__dirname, "public");
 
-// const backendHost = "isa-c4p-ql4s.onrender.com";
-// const backendHost = "// Backend hostname
-
-
 const server = http.createServer((req, res) => {
-  if (req.url.startsWith("/api/doc")) {
-    // Proxy requests for Swagger UI to the backend
-    const options = {
-      hostname: "localhost",
-      port: 8080, // HTTPS default port
-      path: req.url, // Forward the full path
-      method: req.method,
-      headers: req.headers,
-    };
+	let filePath;
+	if (req.url === "/") {
+		filePath = path.join(publicDirectory, "index.html");
+	} else if (req.url === "/main") {
+		filePath = path.join(publicDirectory, "main.html");
+	} else if (req.url === "/admin") {
+		filePath = path.join(publicDirectory, "admin.html");
+	} else if (req.url.startsWith("/reset")) {
+		filePath = path.join(publicDirectory, "password-reset.html");
+	} else {
+		filePath = path.join(publicDirectory, req.url);
+	}
 
-    const proxy = https.request(options, (backendRes) => {
-      res.writeHead(backendRes.statusCode, backendRes.headers);
-      backendRes.pipe(res, { end: true });
-    });
+	const extname = path.extname(filePath);
+	let contentType = "text/html";
+	switch (extname) {
+		case ".js":
+			contentType = "application/javascript";
+			break;
+		case ".css":
+			contentType = "text/css";
+			break;
+		case ".json":
+			contentType = "application/json";
+			break;
+		case ".png":
+			contentType = "image/png";
+			break;
+		case ".jpg":
+			contentType = "image/jpeg";
+			break;
+		default:
+			contentType = "text/html";
+	}
 
-    proxy.on("error", (err) => {
-      console.error("Error proxying request:", err.message);
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Error communicating with backend");
-    });
-
-    req.pipe(proxy, { end: true });
-    return;
-  }
-  let filePath;
-  if (req.url === "/") {
-    filePath = path.join(publicDirectory, "index.html");
-  } else if (req.url === "/main") {
-    filePath = path.join(publicDirectory, "main.html");
-  } else if (req.url === "/admin") {
-    filePath = path.join(publicDirectory, "admin.html");
-  } else if (req.url.startsWith("/reset")) {
-    filePath = path.join(publicDirectory, "password-reset.html");
-  } else {
-    filePath = path.join(publicDirectory, req.url);
-  }
-
-  const extname = path.extname(filePath);
-  let contentType = "text/html";
-  switch (extname) {
-    case ".js":
-      contentType = "application/javascript";
-      break;
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".jpg":
-      contentType = "image/jpeg";
-      break;
-    default:
-      contentType = "text/html";
-  }
-
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code === "ENOENT") {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("<h1>404 - File Not Found</h1>", "utf8");
-      } else {
-        res.writeHead(500);
-        res.end(`Server Error: ${err.code}`, "utf8");
-      }
-    } else {
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content, "utf8");
-    }
-  });
+	fs.readFile(filePath, (err, content) => {
+		if (err) {
+			if (err.code === "ENOENT") {
+				res.writeHead(404, { "Content-Type": "text/html" });
+				res.end("<h1>404 - File Not Found</h1>", "utf8");
+			} else {
+				res.writeHead(500);
+				res.end(`Server Error: ${err.code}`, "utf8");
+			}
+		} else {
+			res.writeHead(200, { "Content-Type": contentType });
+			res.end(content, "utf8");
+		}
+	});
 });
 
 server.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
+	console.log(`Server running on http://localhost:${PORT}`)
 );
